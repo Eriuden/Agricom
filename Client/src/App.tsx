6dda97937e24cf020168f4e441af05660af68f22
Eriuden <antoinecarrette@hotmail.fr>
2025-12-28 11:47:42 +0100

    build: Client App.tsx 28/12/2025

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,4 +1,5 @@
-
+import Header from './Components/Header'
+import { Route,Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -6,7 +7,18 @@ function App() {
 
   return (
     <>
-      
+      <div>
+        <Header/>
+        <Searcher/>
+        <Routes>
+          <Route path={"/"} element={<Home/>}/>
+          <Route path={"/user-profile/:id"} element={<UserProfile/>}/>
+          <Route path={"/reset-password"} element={<ResetPassword/>}/>
+          <Route path={"/update-password"} element={<UpdatePassword/>}/>
+          <Route path={"/article-page/:id"} element={<ArticlePage/>}/>
+          <Route path={"/cart"} element={<Cart/>}/>
+        </Routes>
+      </div>
     </>
   )
 }
