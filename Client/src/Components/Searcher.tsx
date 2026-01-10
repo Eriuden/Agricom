b85369ff2bf780a8856aa34908aa2cabf2e87361
unknown <antoinecarrette@hotmail.fr>
2026-01-10 12:41:52 +0100

    build: searcher component and client utils 10/01/2026

--- Client/src/Components/Searcher.tsx
+++ Client/src/Components/Searcher.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getAllArticles } from "../redux/actions/article.action"
+import { ArticleCard } from "./ArticleCard"
+import { isEmpty } from "../utils"
 
-const Searcher = () => {
+export const Searcher = () => {
+
+    type appDispatch = () => any
+    const [search, setSearch] = useState("")
+    const articles = useSelector((state: any) => state.allArticleReducer)
+    const useAppDispatch = () => useDispatch<appDispatch>()
+    const dispatch = useAppDispatch()
+
+    useEffect(()=> {
+        getAllArticles(articles, dispatch)
+    }, [search])
+
+    const handleSearchChange = (e: any) => {
+        setSearch(e.target.value)
+
+        const result = articles.filter((article: any)=> article.name.toLowerCase()
+        .includes(search.toLowerCase()) 
+        || article.productType.toLowerCase().includes(search.toLowerCase()))
+        setSearch(result)
+    }
   return (
-    <div>
-      
+    <div className='flex justify-center'>
+      <form action='' className='border-2 rounded-sm my-4 max-w-[100%] border-black'>
+        <input className='text-center' type="text" placeholder="Article/type"
+        onChange={handleSearchChange} value={search}/>
+      </form>
+
+      <div>
+        { !isEmpty(articles[0]) && search !="" ? (
+          articles.map((article:any) => {
+            if (article.name.includes(search) || article.productType.includes(search)){
+              <ArticleCard articleProps={article} key={article._id}/>
+            }
+          })
+        ): ""}
+      </div>
     </div>
   )
 }
-
-export default Searcher
