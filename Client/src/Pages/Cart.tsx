5df05aeb63d1c760600a82a4744ddea2a3b69eeb
unknown <antoinecarrette@hotmail.fr>
2026-01-15 14:25:49 +0100

    fix: cart page 15/01/2026

--- Client/src/Pages/Cart.tsx
+++ Client/src/Pages/Cart.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { useSelector} from "react-redux";
 
-const Cart = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+export const Cart = () => {
+    const cartData = useSelector((state: any)=> state.cartReducer)
+
+    return (
+      <div>
+          <div>
+              {cartData.map((article: any)=> {
+                  <>
+                      <div>
+                          <h2>{article.name}</h2>
+                          <img src={article.picture}/>
+                          <h3>{article.productType}</h3>
+                          <h3>{article.price}</h3>
+                      </div>
+                  </>           
+              })}
+          </div>
+  
+          <div>
+              <span>{cartData.totalPrice}</span>
+          </div>
+          
+      </div>
+    )
 }
-
-export default Cart
