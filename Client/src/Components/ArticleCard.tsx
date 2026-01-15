f82981082aa4f4ca38df369d02ce3a662ffed69f
unknown <antoinecarrette@hotmail.fr>
2026-01-15 14:29:03 +0100

    fix: article card 15/01/2026

--- Client/src/Components/ArticleCard.tsx
+++ Client/src/Components/ArticleCard.tsx
@@ -1,10 +1,11 @@
 
-export const ArticleCard = (articleProps: any) => {
+export const ArticleCard = (articleProps: any, userProps:any) => {
   return (
     <div key={articleProps._id} className="flex flex-col">
-      <div> { articleProps._id ? (
+      <div> { articleProps._id && userProps._id ? (
         <>
           <img src={articleProps.picture}/>
+          <h3>{userProps.name}</h3>
           <h3>{articleProps.name}</h3>
           <h3>{articleProps.productType}</h3>
           <h3>{articleProps.price}</h3>
--- Client/src/Pages/Home.tsx
+++ Client/src/Pages/Home.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import {useState, useEffect} from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { ArticleCard } from "../Components/ArticleCard"
+import {getAllArticles} from "../redux/actions/article.action"
+import {isEmpty} from "../utils"
 
-const Home = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+export const Home = () => {
+    const [loadCard, setLoadCard] = useState(false)
+    const [count, setCount] = useState(0)
+    const dispatch = useDispatch()
+    const articles = useSelector((state:any)=> state.allArticleReducer)
+  
+    const loadMore = () => {
+      if (window.innerHeight + document.documentElement.scrollTop + 1 >
+        document.scrollingElement!.scrollHeight)
+        {
+          setLoadCard(true)
+        }
+    }
+  
+    useEffect(()=> {
+      if (loadCard) {
+        getAllArticles(count,dispatch)
+        setLoadCard(false)
+        setCount(count + 10)
+      }
+      window.addEventListener("scroll", loadMore)
+    }, [loadCard, dispatch, count])
+    
+    return (
+      <div>
+        <div>
+          <ul>
+            {!isEmpty(articles[0]) &&
+              articles.map((article:any) => {
+                return <ArticleCard articleProps={article} key={article._id}/>
+            })}
+          </ul>
+        </div>
+      </div>
+    )
 }
-
-export default Home
