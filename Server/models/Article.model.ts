1e038e1dd53fa4e72891e2136d8847fa0f337878
unknown <antoinecarrette@hotmail.fr>
2026-04-12 12:18:43 +0200

    fix: article controller now in ts 02/04/2026

--- Server/models/Article.model.ts
+++ Server/models/Article.model.ts
@@ -1,28 +1,32 @@
+import { Schema, Document, model } from "mongoose";
 const mongoose = require("mongoose")
+
+export interface IArticle extends Document {
+  picture : string;
+  name: string;
+  productType: string;
+  price : string;
+}
 
 const articleSchema = new mongoose.Schema(
     {
         picture: {
-            type: String,
             required: true 
         },
 
         name: {
-            type: String,
             trim: true,
             maxlength: 200,
             required: true,
         },
 
         productType: {
-            type: String,
             trim: true,
             maxlength: 100,
             required: true,
         },
 
         price: {
-            type: String,
             trim: true,
             max: 10,
             required: true,
@@ -32,5 +36,5 @@ const articleSchema = new mongoose.Schema(
     {timestamps : true}
 )
 
-const articleModel = mongoose.model("article", articleSchema)
+const articleModel = model<IArticle>("article", articleSchema)
 module.exports = articleModel
